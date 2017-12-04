3404d9cad4bcef934601e2a45bfb21062c8ecf0f
eldritchjs <jschlessman@gmail.com>
2017-12-04 13:31:19 -0500

    Syntax issue app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 import time
 import stomp
 
-parser = argparse.ArgumentParser(description='AMQ sales sender')
+parser = argparse.ArgumentParser(description='AMQ words sender')
 parser.add_argument('--servers', help='The AMQP server', default='broker-amq-stomp')
 parser.add_argument('--port', help = 'The AMQP port', default='61613')
 parser.add_argument('--queue', help='Queue to publish to', default='salesq')
@@ -14,7 +14,7 @@ parser.add_argument('--count', type=int, help='Total records to publish', defaul
 args = parser.parse_args()
 
 server = os.getenv('SERVERS', args.servers)
-port = os.getenv('PORT', args.port)
+port = int(os.getenv('PORT', args.port))
 topic = os.getenv('QUEUE', args.topic)
 rate = int(os.getenv('RATE', args.rate))
 count = int(os.getenv('COUNT', args.count))
@@ -23,7 +23,8 @@ count = int(os.getenv('COUNT', args.count))
 
 #producer = KafkaProducer(bootstrap_servers=servers)
 dest = '/queue' + queue
-c = stomp.Connection([(server, int(port)])
+c = stomp.Connection([(server, port]))
+c.start()
 c.connect('daikon', 'daikon', wait=True)
 
 with gzip.open('words.gz', 'r') as f:
